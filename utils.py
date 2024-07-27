2e57fb8c09ed22cde6e30beba4da8fdb084ab0a9
25095 <2509562097@qq.com>
2024-07-27 23:23:37 +0800

    update utils

--- utils.py
+++ utils.py
@@ -61,6 +61,7 @@ class Accumulator:
 def try_gpu(i=0):
     """Return gpu(i) if exists, otherwise return cpu().
     Defined in :numref:`sec_use_gpu`"""
+
     if torch.cuda.device_count() >= i + 1:
         return torch.device(f'cuda:{i}')
     return torch.device('cpu')
